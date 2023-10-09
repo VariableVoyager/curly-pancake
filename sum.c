f6e13e9942d28a19bacf2ef44eab896deca14b0e
Soumabha Bhim <136871204+sigma-coder@users.noreply.github.com>
2023-10-09 09:35:03 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x, y;
+    printf("Enter the value of x\n");
+    scanf("%d", &x);
+
+    printf("Enter the value of y\n");
+    scanf("%d", &y);
+
+    printf("The sum of x and y is %d\n", x+y);
+      
+    return 0;
+}
